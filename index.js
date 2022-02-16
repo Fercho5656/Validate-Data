a652a5e0d8e6c2397b15359f16ad412f0ea7fa94
Fercho5656 <morenomoranfernando@gmail.com>
2022-02-16 08:37:35 -0600

    Completed validation

--- index.js
+++ index.js
@@ -1,45 +1,58 @@
-const $ = e => document.querySelector(e);
-const $$ = e => document.querySelectorAll(e);
+const $ = (e) => document.querySelector(e);
 
-const dataTypes = {
-    'string': 'string',
-    'number': 'number',
-    'boolean': 'boolean',
-}
+const form = $("#dataForm");
 
-const form = $('#dataForm');
-
-const phone = $('#phone');
-const fullname = $('#fullname');
-const zipCode = $('#zipCode');
-const salary = $('#salary');
-const observations = $('#observations');
-const age = $('#age');
-const studyLevel = $('#studyLevel');
-const isAlive = $('#isAlive');
-//const validateInfoBtn = $('#validateInfo');
+const phone = $("#phone");
+const fullname = $("#fullname");
+const zipCode = $("#zipCode");
+const salary = $("#salary");
+const observations = $("#observations");
+const age = $("#age");
+const studyLevel = $("#studyLevel");
+const isAlive = $("#isAlive");
 
 const data = [
-    phone,
-    fullname,
-    zipCode,
-    salary,
-    observations,
-    age,
-    studyLevel,
-    isAlive
-]
+  phone,
+  fullname,
+  zipCode,
+  salary,
+  observations,
+  age,
+  studyLevel,
+  isAlive,
+];
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-    !isNaN(+phone.value) ? phone.classList.remove('error') : phone.classList.add('error');
-    /^[a-zA-Z()]+$/.test(fullname.value) ? fullname.classList.remove('error') : fullname.classList.add('error');
-    !isNaN(+zipCode.value) ? zipCode.classList.remove('error') : zipCode.classList.add('error');
-    !isNaN(+salary.value) ? salary.classList.remove('error') : salary.classList.add('error');
-    !isNaN(+age.value) ? age.classList.remove('error') : age.classList.add('error');
-    data.any(e => e.classList.contains('error')) ? alert('Por favor, verifique los campos.') : alert('Dados validados con éxito!');
+//validation
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validateNumber(phone.value)
+    ? phone.classList.remove("error")
+    : phone.classList.add("error");
+
+  /^[a-zA-Z()]+$/.test(fullname.value)
+    ? fullname.classList.remove("error")
+    : fullname.classList.add("error");
+
+  validateNumber(zipCode.value)
+    ? zipCode.classList.remove("error")
+    : zipCode.classList.add("error");
+
+  validateNumber(salary.value)
+    ? salary.classList.remove("error")
+    : salary.classList.add("error");
+
+  validateNumber(age.value)
+    ? age.classList.remove("error")
+    : age.classList.add("error");
+
+  validateNumber(studyLevel.value)
+    ? studyLevel.classList.remove("error")
+    : studyLevel.classList.add("error");
+
+  data.some((e) => e.classList.contains("error"))
+    ? alert("Por favor, verifique los campos.")
+    : alert("Dados validados con éxito!");
 });
 
-const validateType = (data, type) => {
-    return typeof data === type;
-}
+const validateNumber = (number) => !isNaN(+number) && number.length > 0;
